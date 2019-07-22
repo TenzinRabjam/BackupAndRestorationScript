7cf35e4de2a4d70651092845cad6e0cd8ab017e3
TenzinRabjam <46458255+TenzinRabjam@users.noreply.github.com>
2019-07-22 16:29:08 -0600

    Add files via upload
    updated commit

---
+++ powershellScriptForBackupAndRestore.ps1
@@ -0,0 +1,16 @@
+﻿//Run as admin mode
+
+Import-Module sqlps
+
+Backup-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "midterm"
+
+Backup-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "MafiaWorks"
+
+Backup-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "WorldWideImporters"
+
+Backup-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "WorldWideImportersDW"
+
+Backup-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "SSISDB"
+
+//To Restore
+Restore-SqlDatabase -ServerInstance "TRABJAM1995\MSSQLSERVER01" -Database "midterm"
